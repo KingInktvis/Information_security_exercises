d717a1d59e591f40bba99a26546fc79c4b1c7f70
KingInktvis <rikdevries253@hotmail.nl>
2017-09-27 16:10:40 +0200

    Exercice 12 file

---
+++ src/chapter3/exercise12.java
@@ -0,0 +1,18 @@
+package chapter3;
+
+public class exercise12 {
+    private int base = 43210;
+    private int power = 23456;
+    private int modulo = 99987;
+
+    public exercise12() {
+    }
+
+    public void generate() {
+
+    }
+
+    public static void main(String[] args) {
+        new exercise12();
+    }
+}
